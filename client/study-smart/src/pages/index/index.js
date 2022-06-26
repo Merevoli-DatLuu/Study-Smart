7d068fdcfb282a3b4bd15b980b26f9a9da76de70
Bug03 <90082574+Bug03@users.noreply.github.com>
2022-06-26 19:09:40 +0700

    [Add]: Interface Pomodoro

--- client/study-smart/src/pages/index/index.js
+++ client/study-smart/src/pages/index/index.js
@@ -1,7 +1,25 @@
 Page({
   onLoad(query) {
+   
   },
   onReady() {
+    console.log("onload")
+   
+    var numb=   document.querySelector(".numb").exec((ret) => {
+      console.dir(ret);
+
+    });
+   // console.dir(numb);
+      numb.value="100%";
+            let counter = 0;
+            // setInterval(()=>{
+            //   if(counter == 100){
+            //     clearInterval();
+            //   }else{
+            //     counter+=1;
+            //     numb.textContent = counter + "%";
+            //   }
+            // }, 80);
   },
   onShow() {
   },
