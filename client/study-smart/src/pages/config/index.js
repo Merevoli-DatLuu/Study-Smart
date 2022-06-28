943beb60222f7ad5d94679f80da1f1a980e75975
Trung kien <trungkien123456k@gmail.com>
2022-06-28 11:51:50 +0700

    split html/css and structure folder client

--- client/study-smart/src/pages/config/index.js
+++ client/study-smart/src/pages/config/index.js
@@ -1,4 +1,8 @@
 Page({
+  data: {
+    value: 10
+  },
+
   onLoad(query) {
   },
   onReady() {
---
+++ client/study-smart/src/pages/timer/index.js
@@ -0,0 +1,19 @@
+Page({
+  data:{
+    button : [
+      {icon : 'refresh'},
+      {icon : 'play_glyph'},
+      {icon : 'skip_next_glyph'}
+    ]
+  },
+  onLoad(query) {
+  },
+  onReady() {
+  },
+  onShow() {
+  },
+  onHide() {
+  },
+  onUnload() {
+  }
+});
