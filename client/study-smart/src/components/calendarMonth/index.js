86fdcc9639bc9bec2c5f77af9f65a9ff4b24d808
Bug03 <90082574+Bug03@users.noreply.github.com>
2022-06-29 13:31:41 +0700

    [Add]: calendar and banner

---
+++ client/study-smart/src/components/calendarMonth/index.js
@@ -0,0 +1,30 @@
+Component({
+  mixins: [], // các minxin sử dụng trong Component
+  data: { x: 1 }, // data của component
+  props: { y: 1 }, // các thuộc tính
+
+  // các hàm life cycles
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+
+  // các methods
+  methods: {
+  
+  }
+});
+Component({
+  mixins: [], // các minxin sử dụng trong Component
+  data: { x: 1 }, // data của component
+  props: { y: 1 }, // các thuộc tính
+
+  // các hàm life cycles
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+
+  // các methods
+  methods: {
+  
+  }
+});
---
+++ client/study-smart/src/pages/calendar/index.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+   value:10
+  },
+  onLoad(query) {
+  },
+  onReady() {
+  },
+  onShow() {
+  },
+  onHide() {
+  },
+  onUnload() {
+  },
+  onRestoreDefault(){
+    this.setData({value:0})
+  }
+});
